6c65abc5de5129c9626f0f7a2150da96fce0adc6
mailgurudev <mailgurudev@gmail.com>
2018-12-22 09:32:26 -0800

    Pallindrome

--- number_reverse.py
+++
@@ -1,7 +0,0 @@
-def num_reverse(number):
-    number = input ( "Enter the number : " )
-    result = number.split ( " " )
-    new_result = [ ]
-    for i in reversed ( result ):
-        new_result.append ( i )
-        final_result = " ".join ( new_result )
